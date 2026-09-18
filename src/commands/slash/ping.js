4105561ddbd53e67891295dcd63ce8744491abbe
punagamer32 <punagamer32@gmail.com>
2026-09-18 13:24:54 -0600

    Add ping command with integration and context types

--- src/commands/slash/ping.js
+++ src/commands/slash/ping.js
@@ -1,1 +1,20 @@
+const { SlashCommandBuilder, ApplicationIntegrationType, InteractionContextType } = require('discord.js');
 
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('ping')
+    .setDescription('Replies with Pong!')
+    .setIntegrationTypes(
+      ApplicationIntegrationType.GuildInstall,
+      ApplicationIntegrationType.UserInstall
+    )
+    .setContexts(
+      InteractionContextType.Guild,
+      InteractionContextType.BotDM,
+      InteractionContextType.PrivateChannel
+    ),
+
+  async execute(interaction) {
+    await interaction.reply('🏓 Pong!');
+  },
+};
