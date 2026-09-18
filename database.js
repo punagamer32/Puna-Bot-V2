ae78e7554e2a1f0d80cba9e3086867b9e3c36eff
punagamer32 <punagamer32@gmail.com>
2026-09-18 13:19:46 -0600

    Refactor database.js to only export connectDB function
    Removed guild configuration schema and Discord client setup from database.js.

--- database.js
+++ database.js
@@ -1,4 +1,4 @@
-const { Schema, model, mongoose } = require('mongoose');
+const mongoose = require('mongoose');
 
 async function connectDB() {
   try {
@@ -11,36 +11,3 @@ async function connectDB() {
 }
 
 module.exports = { connectDB };
-
-const guildConfigSchema = new Schema({
-  guildId: { type: String, required: true, unique: true },
-  prefix: { type: String, default: '!' },
-  welcomeChannel: String,
-  createdAt: { type: Date, default: Date.now },
-});
-
-module.exports = model('GuildConfig', guildConfigSchema);
-
-require('dotenv').config();
-const { Client, GatewayIntentBits, Collection } = require('discord.js');
-const { connectDB } = require('./database');
-const { loadCommands } = require('./handlers/commandHandler');
-const { loadEvents } = require('./handlers/eventHandler');
-
-const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent, // Required for prefix commands
-  ],
-});
-
-client.commands = new Collection();
-client.prefixCommands = new Collection();
-
-(async () => {
-  await connectDB();
-  await loadCommands(client);
-  await loadEvents(client);
-  await client.login(process.env.DISCORD_TOKEN);
-})();
